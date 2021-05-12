28af6690a50776e4688844281aa3d187a592072a
Ashwin Giri <ashgiri@publicisgroupe.net>
2021-05-12 16:50:24 +0530

    Updates

---
+++ precache-manifest.1428c5c62f175b9a95a68fba710d1e38.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d62d61e1df5e357aa274cddef5c4e95a",
+    "url": "/scrum-board/index.html"
+  },
+  {
+    "revision": "aafa668cac15cc9092f7",
+    "url": "/scrum-board/static/css/2.17f8cf15.chunk.css"
+  },
+  {
+    "revision": "91bab00d5ae8bbdb5530",
+    "url": "/scrum-board/static/css/main.8c3f1ea7.chunk.css"
+  },
+  {
+    "revision": "aafa668cac15cc9092f7",
+    "url": "/scrum-board/static/js/2.7aed8796.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/scrum-board/static/js/2.7aed8796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91bab00d5ae8bbdb5530",
+    "url": "/scrum-board/static/js/main.0585ddc7.chunk.js"
+  },
+  {
+    "revision": "a14ff5e9de74c6a23f1d",
+    "url": "/scrum-board/static/js/runtime-main.32e3e670.js"
+  },
+  {
+    "revision": "11c2cd068a4bb8c39a638bb2f56eb0b1",
+    "url": "/scrum-board/static/media/fontello.11c2cd06.woff2"
+  },
+  {
+    "revision": "7307269ecde5ef26aae9e8ce53a80280",
+    "url": "/scrum-board/static/media/fontello.7307269e.svg"
+  },
+  {
+    "revision": "9975fb69b9f2e33b1099b5968a6916a1",
+    "url": "/scrum-board/static/media/fontello.9975fb69.eot"
+  },
+  {
+    "revision": "a108f659bcc06170b618c1be53213973",
+    "url": "/scrum-board/static/media/fontello.a108f659.woff"
+  },
+  {
+    "revision": "e08f4fdc5cbdc1b98f29fd06e6afd7ba",
+    "url": "/scrum-board/static/media/fontello.e08f4fdc.ttf"
+  }
+]);
